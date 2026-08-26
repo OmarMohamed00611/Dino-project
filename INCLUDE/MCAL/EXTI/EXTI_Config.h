7f200f5dfda037a2afc09ea6c767d9ef77743067
amrfawzy779 <amrfwa140@gmail.com>
2026-08-26 20:48:13 +0300

    Delete INCLUDE directory

--- INCLUDE/MCAL/EXTI/EXTI_Config.h
+++
@@ -1,5 +0,0 @@
-#ifndef INCLUDE_MCAL_EXTI_EXTI_CONFIG_H_
-#define INCLUDE_MCAL_EXTI_EXTI_CONFIG_H_
-
-#endif
-
--- INCLUDE/MCAL/GI/GI_Config.h
+++
@@ -1,6 +0,0 @@
-
-#ifndef INCLUDE_MCAL_GI_GI_CONFIG_H_
-#define INCLUDE_MCAL_GI_GI_CONFIG_H_
-
-#endif
-
